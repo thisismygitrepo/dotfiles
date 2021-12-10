3f81aba8617842081db512ca74e34ab4f3d79310
alex <programmer@usa.com>
2021-12-10 15:26:30 +1030

    renamed

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -1,22 +1,28 @@
 
-@REM IDEs
+# 1- Getting Started
+winget install --name "Google Chrome" --Id "Google.Chrome" --source winget
+winget install --name "Chrome Remote Desktop Host" --Id "Google.ChromeRemoteDesktop" --source winget
+# 2- Sign in to Chrome with G account, this will give access to LastPass, sign in to that as well.
+
+# IDEs
 winget install --name "anaconda3"
 winget install --name "PyCharm Professional Edition"
 winget install --name "Node.js"
 winget install --name "julia" --Id "Julialang.Julia 1.6.2"
 winget install --name "DB Browser for SQLite"
 
-@REM Python: add conda to PATH, config terminals,
+# Python: add conda to PATH, config terminals,
 set PATH=%PATH%;C:\ProgramData\Anaconda3
 set PATH=%PATH%;C:\ProgramData\Anaconda3\Scripts
-@REM # if windows-terminal doesn't accept modification, run this:
+# # if windows-terminal doesn't accept modification, run this:
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
 conda init powershell
-@REM pwsh -Command "conda init powershell"
+# pwsh -Command "conda init powershell"
 conda create --name ve python=3.9
 
-@REM git repos:
+# git repos:
 winget install --name "Git"
+mkdir code
 cd code
 git clone https://github.com/thisismygitrepo/crocodile.git
 pip install -e ./crocodile
@@ -26,7 +32,7 @@ python create_symlinks.py
 git clone https://github.com/thisismygitrepo/crypto.git
 pip install -r ./crypto/requirements.txt
 
-@REM Shells
+# Shells
 winget install --name "notepad++"
 winget install --name "PuTTY"
 winget install --name "AWS Command Line Interface"
@@ -36,7 +42,7 @@ winget install JanDeDobbeleer.OhMyPosh
 python "terminal_setup/install_fancy_terminal.py"
 python "terminal_setup/change_terminal_settings.py"
 
-@REM productivity
+# productivity
 winget install --name "Mozilla Firefox"
 winget install --name "Mozilla Thunderbird"
 winget install --name "Microsoft Garage Mouse without Borders"
