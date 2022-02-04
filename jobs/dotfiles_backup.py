0710832334c236e65935765afeaca0e208d645b4
alex <programmer@usa.com>
2022-02-04 12:08:13 +1030

    made consistent jobs

---
+++ jobs/dotfiles_backup.py
@@ -0,0 +1,24 @@
+
+import crocodile.toolbox as tb
+
+dat = tb.P.home().joinpath("dotfiles")
+
+
+def main():
+    """Encrypts and saves a copy of `dotfiles` to OneDrive"""
+    key = dat.joinpath("creds/encrypted_files_key.bytes")
+    downloaded_key_from_lastpass = False
+    if not key.exists():
+        # key = input(f"Pass key if you have an old one, or press enter to create a new one")
+        key = tb.P.home().joinpath("Downloads/key.zip").unzip(inplace=False, verbose=True).find()
+        downloaded_key_from_lastpass = True
+    # P("lastpass_export_2022_1_1.csv").encrypt(key="key.zip")
+    # caveat: if there is more than onedrive account.
+    dat.zip_n_encrypt(key=key, inplace=False, verbose=True).move(tb.P(tb.os.environ["ONEDRIVE"]).joinpath("AppData"),
+                                                                 overwrite=True)
+    if downloaded_key_from_lastpass: key.delete(sure=True)
+    print(" ========================= SUCCESSFULLY BACKEDUP ===============================")
+
+
+if __name__ == '__main__':
+    pass
