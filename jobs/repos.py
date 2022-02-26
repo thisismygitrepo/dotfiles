d87d100a3495e379802e7fb40c253f5551ba72ca
alex <programmer@usa.com>
2022-02-27 09:48:36 +1030

    auto_commit_aCj1pnZfk0

--- jobs/repos.py
+++ jobs/repos.py
@@ -7,16 +7,21 @@ tm = tb.Terminal()
 
 
 def commit_one(path, mess="auto_commit_" + tb.randstr()):
+    print(f"Committing {path}".center(80, "-"))
     res = tm.run(f'cd {path}; git add .; git commit -am "{mess}"', shell="powershell")
+    res.print()
     remotes = tm.run("git remote", shell="powershell").op.split("\n")
     for remote in remotes:
-        tm.run(f'git push {remote}')
+        res = tm.run(f'git push {remote}')
+        res.print()
 
 
 def pull_one(path):
+    print(f"Pulling {path}".center(80, "-"))
     remotes = tm.run("git remote", shell="powerpsshell").op.split("\n")
     for remote in remotes:
-        tm.run(f'cd {path}; git pull {remote}', shell="powershell")
+        res = tm.run(f'cd {path}; git pull {remote}', shell="powershell")
+        res.print()
 
 
 def commit_all():
