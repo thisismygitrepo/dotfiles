56b9d743dbde60ec7a4bbd17e6e10eefc29a53e1
Alex Al-Saffar <programmer@usa.com>
2022-03-12 21:39:22 +1030

    added arguments to activation of ve

---
+++ jobs/backup_retrieve.py
@@ -0,0 +1,25 @@
+
+
+import crocodile.enviroment as env
+from jobs.utils import retrieve as retrieve_func, backup as backup_func
+
+
+def bacup_dotfiles():
+    backup_func("~/dotfiles")
+
+
+def retrieve_dotfiles():
+    retrieve_func(source_file="dotfiles_encrypted.zip", target_folder="~")
+
+
+def backup_thunderbird():
+    path = env.AppData / "Thunderbird" / "Profiles" / "h7omvr4i.default-release"
+    backup_func(path)
+
+
+def retrieve_thunderbird():
+    retrieve_func(source_file="thunderbird_encrypted.zip", target_folder=env.AppData / "Thunderbird" / "Profiles")
+
+
+if __name__ == '__main__':
+    pass
--- scripts/windows/dotfiles_backup.ps1
+++ scripts/windows/dotfiles_backup.ps1
@@ -1,3 +1,3 @@
 ~/venvs/ve/Scripts/Activate.ps1
 cd ~/code/dotfiles
-python -m fire ./jobs/dotfiles_backup.py main
+python -m fire ./jobs/backup_retrieve.py backup_dotfiles
