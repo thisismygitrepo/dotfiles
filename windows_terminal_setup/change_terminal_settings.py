d8bfb66bb6bede6daf5f5c728dd605ec492f46b1
Alex Al-Saffar <programmer@usa.com>
2022-03-12 15:05:47 +1030

    aded symlink file

--- windows_terminal_setup/change_terminal_settings.py
+++ windows_terminal_setup/change_terminal_settings.py
@@ -5,11 +5,10 @@ from uuid import uuid4
 
 """
 Not to be confused:
-Windows Terminal & Windows Terminal Preview
-The latter is the night release version of WT.
-Windows PowerShell & PowerShell.
-The latter is community developed and is available on all platforms.
-Lastly, powershell has a preview version as well.
+* Windows Terminal & Windows Terminal Preview: The latter is the night release version of WT.
+* Windows PowerShell & PowerShell: The latter is community developed and is available on all platforms.
+* Lastly, powershell has a preview version as well.
+All modules are available on GitHub: https://aka.ms/terminal-profiles-schema
 """
 
 
